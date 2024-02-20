e54e2de193d8e013d8d8105d0d2b0c9552518d6b
keval vaja <120974030+Kevalvaja@users.noreply.github.com>
2024-02-20 16:58:32 +0530

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 
 app.use("/", (req, res) => {
-  res.status(200).json("Hello world 4848");
+  res.status(200).json("Hello world 97238");
 });
 
 app.listen(3000, () => {
