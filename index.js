264b2600b9cb7f3c6bd304eeb4929172bb367e86
keval vaja <120974030+Kevalvaja@users.noreply.github.com>
2024-02-20 15:30:41 +0530

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 
 app.use("/", (req, res) => {
-  res.status(200).json("Hello kishan");
+  res.status(200).json("This is my first jenkins through deploy");
 });
 
 app.listen(3000, () => {
