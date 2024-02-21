fa4254664952e5e670bc1a44b6454a64d689b8ca
keval vaja <120974030+Kevalvaja@users.noreply.github.com>
2024-02-21 15:27:08 +0530

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 
 app.use("/", (req, res) => {
-  res.status(200).json("Hello world runing my docker through build");
+  res.status(200).json("Hello world runing my docker through build ?");
 });
 
 app.listen(3000, () => {
